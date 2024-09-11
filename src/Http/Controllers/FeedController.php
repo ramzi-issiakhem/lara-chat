30c46b2dad78e3ed7c64d33f7b9c55a4730185d7
Ramzi Issiakhem <46163558+ramzi-issiakhem@users.noreply.github.com>
2024-09-11 23:41:37 +0000

    Implemented Base API Routes (#3)

---
+++ src/Http/Controllers/FeedController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ramzi\LaraChat\Http\Controllers;
+
+class FeedController extends BaseController
+{
+
+    // Retrieve the Feed with the
+    public function index() {
+
+    }
+
+}
