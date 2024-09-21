31c8ac06f1e0287e52169513160d283ba754b48f
ramzi <issiakhem.mohamedramzi@gmail.com>
2024-09-21 11:39:04 +0100

    Modified and optimized the Models to simplify the structure

--- src/Models/Participant.php
+++ src/Models/Participant.php
@@ -2,20 +2,35 @@
 
 namespace Ramzi\LaraChat\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Ramzi\LaraChat\Traits\ManageCustomUserModel;
 
 class Participant extends Model
 {
 
+    use ManageCustomUserModel;
+
     protected $table = 'participants';
+
+    /**
+     * Return the Relationship representing the Thread related to this Participant
+     * @return BelongsTo
+     */
     public function thread() : BelongsTo {
         return $this->belongsTo(Thread::class);
     }
 
-    public function participantable() : morphTo {
-        return $this->morphTo();
+    /**
+     * Return the Relationship representing the Sender Model related to this Participant
+     * @return BelongsTo
+     */
+    public function sender(): BelongsTo
+    {
+        $modelClass = $this->getLaraChatSenderModel();
+        return $this->belongsTo($modelClass, "sender_id");
     }
-
 }
--- src/Models/ThreadSeen.php
+++ src/Models/ThreadSeen.php
@@ -2,24 +2,44 @@
 
 namespace Ramzi\LaraChat\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Ramzi\LaraChat\Facades\LaraChat;
+use Ramzi\LaraChat\Traits\ManageCustomUserModel;
 
 class ThreadSeen extends Model
 {
+    use ManageCustomUserModel;
 
-    protected $table = 'thread_seens';
+    protected $table = 'threads_seen';
+
+    /**
+     * Return the Relationship representing the Thread related to this ThreadSeen
+     * @return BelongsTo
+     */
     public function thread() : BelongsTo {
         return $this->belongsTo(Thread::class);
     }
 
-    public function seenable() : morphTo {
-        return $this->morphTo();
+    /**
+     * Return the Relationship representing the Sender Model related to this ThreadSeen
+     * @return BelongsTo
+     */
+    public function sender(): BelongsTo
+    {
+        $userModel = $this->getLaraChatSenderModel();
+        return $this->belongsTo($userModel, "sender_id");
     }
 
-    public function lastMessageSeen() : BelongsTo {
-        return $this->belongsTo(Message::class, 'last_message_seen_id');
+    /**
+     * Return the Relationship representing the Last Message seen
+     * @return BelongsTo
+     */
+    public function lastSeenMessage(): BelongsTo
+    {
+        return $this->belongsTo(Message::class, 'last_seen_message_id');
     }
-
 }
